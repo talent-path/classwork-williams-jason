e68debef9683a6b0740023aba48761cf44b55d8b
Jason-Williams-github <jwilliams@talentpath.com>
2021-02-19 17:56:23 -0500

    end of day push

--- Dota2Api/dota2.js
+++ Dota2Api/dota2.js
@@ -1,31 +1,35 @@
 //63206383
 //test
 let div = document.createElement("div")
-let heroes = ["anti-mage","axe","bane","bloodseeker","crystal maiden",
-            "drow ranger","earthshaker","juggernaught","mirana",
-            "morphling","shadow fiend","phantom lancer","puck","pudge",
-            "razor","sand king","storm spirit","sven","tiny",
-            "vengeful spirit","windranger","zeus","kunkka","BLANK","lina",
-            "lich","lion","shadow shaman","slardar","tidehunter",
-            "witch doctor","riki","enigma","tinker","sniper","necrophos",
-            "warlock","beastmaster","queen of pain","venomancer",
-            "faceless void","wraith king","death prophet",
-            "phantom assassin","pugna","templar assassing",
-            "viper","luna","dragon knight","dazzle","clockwerk",
-            "leshrac","nature's prophet","lifestealer","dark seer",
-            "clinkz","omniknight","enchantress","huskar","night stalker",
-            "broodmother","bounty hunter","weaver","jakiro","batrider",
-            "chen","specter","doom","ancient apparition","ursa",
-            "spirit breaker","gyrocopter","alchemist","invoker","silencer",
-            "outworld devourer","lycan","brewmaster","shadow demon",
-            "lone druid","chaos knight","meepo","treant protector",
-            "ogre magi","undying","rubick","disruptor","nyx assassin",
-            "naga siren","keeper of the light","io","visage",
-            "slark","medusa","troll warlord","centaur warrunner",
-            "magnus","timbersaw","bristleback","tusk","skywrath mage",
-            "abaddon","elder titan","legion commander","ember spirit",
-            "earth spirit","abyssal underlord","terrorblade","phoenix",
-            "techies","oracle","winter wyvern","Arc warden"]
+// let heroes = ["blank","anti-mage","axe","bane","bloodseeker","crystal maiden",
+//             "drow ranger","earthshaker","juggernaught","mirana",
+//             "morphling","shadow fiend","phantom lancer","puck","pudge",
+//             "razor","sand king","storm spirit","sven","tiny",
+//             "vengeful spirit","windranger","zeus","kunkka","BLANK","lina",
+//             "lich","lion","shadow shaman","slardar","tidehunter",
+//             "witch doctor","riki","enigma","tinker","sniper","necrophos",
+//             "warlock","beastmaster","queen of pain","venomancer",
+//             "faceless void","wraith king","death prophet",
+//             "phantom assassin","pugna","templar assassing",
+//             "viper","luna","dragon knight","dazzle","clockwerk",
+//             "leshrac","nature's prophet","lifestealer","dark seer",
+//             "clinkz","omniknight","enchantress","huskar","night stalker",
+//             "broodmother","bounty hunter","weaver","jakiro","batrider",
+//             "chen","specter","doom","ancient apparition","ursa",
+//             "spirit breaker","gyrocopter","alchemist","invoker","silencer",
+//             "outworld devourer","lycan","brewmaster","shadow demon",
+//             "lone druid","chaos knight","meepo","treant protector",
+//             "ogre magi","undying","rubick","disruptor","nyx assassin",
+//             "naga siren","keeper of the light","io","visage",
+//             "slark","medusa","troll warlord","centaur warrunner",
+//             "magnus","timbersaw","bristleback","tusk","skywrath mage",
+//             "abaddon","elder titan","legion commander","ember spirit",
+//             "earth spirit","abyssal underlord","terrorblade","phoenix",
+//             "techies","oracle","winter wyvern","Arc warden"]
+
+
+
+
 const getProfileInfo = function(){
 
     let playerId = $("#accountId").val();
@@ -37,28 +41,27 @@ const getProfileInfo = function(){
         function(data, textStatus , jqXHR ){
             $("#reportHeader").text(`Account Report for ${data.profile.personaname}`)
             $("#profileIcon").attr("src",data.profile.avatarfull)
-            $("#profileInfo").text(`MMR Estiamte: ${data.mmr_estimate.estimate}, 
-                                    Last login: ${data.profile.last_login},
+            $("#profileInfo").text(`MMR Estiamte: ${data.mmr_estimate.estimate},  
+                                    Last login: ${data.profile.last_login}, 
                                     User since ${data.tracked_until}`)
             
 
-            console.log( data )
-            console.log( textStatus )
-            console.log( jqXHR )
+            // console.log( data )
+            // console.log( textStatus )
+            // console.log( jqXHR )
         }
 
         
     )
 
-    for(let i=0;i<heroes.length;i++){
-        console.log(i+" " + heroes[i])
-    }
+    $("#recentMatches").text("");
 }
 
 const getRecentGames = function(){
 
     let playerIdForMatch = $("#accountId").val();
-    $("#recentMatches").text("");
+    $("#recentMatches").text("Your 20 most recent games");
+    
     for(let i=0;i<20;i++){
         
         $.get(
@@ -71,21 +74,22 @@ const getRecentGames = function(){
             $("#recentMatches").append("Match " + (i+1) + " ")
             $("#recentMatches").append(`Match id: ${data[i].match_id} `)
             $("#recentMatches").append(`Match duration: ${data[i].duration} `)
-            $("#recentMatches").append(`Hero : ` + heroes[data[i].hero_id-1])
+            $("#recentMatches").append(`Hero Id :  ${data[i].hero_id} `)
             $("#recentMatches").append(`kills: ${data[i].kills} `)
             $("#recentMatches").append(`death: ${data[i].deaths} `)
             $("#recentMatches").append(`assists: ${data[i].assists} `)
+            $("#recentMatches").append(`K/D: ` + (data[i].kills/data[i].deaths))
             $("#recentMatches").append(``)
+                
+        
+             console.log( data )
+            // console.log( textStatus )
+            // console.log( jqXHR )
 
             
-
-            console.log( data )
-            console.log( textStatus )
-            console.log( jqXHR )
 
 
             }
         )
     }
-
 }
